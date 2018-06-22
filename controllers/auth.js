3a4926128073e2f9f6e33b7cbf868df6bbb9927d
Jacob Spade <jacobspade@Jacobs-MacBook-Pro.local>
2018-06-22 09:26:15 -0700

    fixed validation alerts

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,61 @@
 var express = require('express');
+var db = require('../models');
+var passport = require('../config/passportConfig');
 var router = express.Router();
 
+// GET /auth/signup - send the form for signup
 router.get('/signup', function(req, res) {
   res.render('auth/signup');
 });
 
+// GET /auth/login - send the form to login
 router.get('/login', function(req, res) {
   res.render('auth/login');
+});
+
+// POST /auth/signup - the route that processes the signup form
+router.post('/signup', function(req, res) {
+  // this looks up the user in the DB
+  db.user.findOrCreate({
+    where: { email: req.body.email},
+    defaults: {
+      name: req.body.name,
+      password: req.body.password
+    }
+  }).spread(function(user, created) {
+    if (created) {
+      //No record was found, so we created one
+      passport.authenticate('local', {
+        successRedirect: '/',
+        successFlash: 'Account created and logged in!'
+      })(req, res);
+    } else {
+      // We found a record, so they can't use that email
+      req.flash('error', 'Email already exists!');
+      res.redirect('/auth/signup');
+    }
+  }).catch(function(error) {
+    // catch any additional errors
+    console.log(error.message);
+    req.flash('error', error.message);
+    res.redirect('/auth/signup');
+  });
+});
+
+// POST /auth/login - the route that processes the logn form
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/auth/login',
+  successFlash: 'you have logged in!',
+  failureFlash: 'Invalid username and/or password!'
+}));
+
+// GET /auth/logout - the route that logs you out
+router.get('/logout', function(req, res) {
+  // passport logout() removes req.user and clears the session
+  req.logout();
+  req.flash('success', 'you have logged out!');
+  res.redirect('/');
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+require('dotenv').config();
 var express = require('express');
 var ejsLayouts = require('express-ejs-layouts');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var passport = require('./config/passportConfig');
+var isLoggedIn = require('./middleware/isLoggedIn');
+var flash = require('connect-flash');
+
 var app = express();
 
 app.set('view engine', 'ejs');
@@ -9,11 +15,33 @@ app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
 
+// 1. this needs to come before you app.use passport
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+
+// 2. setup flash messages
+app.use(flash());
+
+// 3. this must come after the session setup
+app.use(passport.initialize());
+app.use(passport.session());
+
+// 4. attach the current user to the response for all routes
+// also, attach the flash messages
+app.use(function(req, res, next) {
+  res.locals.alerts = req.flash();
+  res.locals.currentUser = req.user;
+  next();
+});
+
 app.get('/', function(req, res) {
   res.render('index');
 });
 
-app.get('/profile', function(req, res) {
+app.get('/profile', isLoggedIn, function(req, res) {
   res.render('profile');
 });
 
